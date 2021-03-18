3ca277c57bd2483b89a0679d1e8160297173a7ef
nomikhan7777 <khannuman843@gmail.com>
2021-03-18 15:31:42 +0500

    assignment 3

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace nomi_khan
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
